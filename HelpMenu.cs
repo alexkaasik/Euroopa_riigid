4e70196eea10e9aebe67da2fc6cb58f4a26d6fdd
alexkaasik <aleks.kaasik@outlook.com>
2024-05-02 11:39:50 +0300

    Add project files.

---
+++ HelpMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Euroopa_riigid
+{
+    class Functions
+    {
+        public static void Help()
+        {
+            Console.WriteLine("f = Find");
+            Console.WriteLine("s = Show");
+            Console.WriteLine("e = Edit");
+            Console.WriteLine("g = Game");
+            Console.WriteLine("q = Quit");
+            Console.WriteLine("c = save");
+        }
+
+        public static void Show(Dictionary<string, string> item)
+        {
+            foreach (var value in item) {
+                Console.WriteLine(value.Key + ":" + value.Value);
+            }
+        }
+
+        public static void Find(Dictionary<string, string> item)
+        {
+            Console.Write("Enter a word:");
+            string sona = Console.ReadLine().ToLower();
+            if ( dict.ContainsKey(sone) ) { } 
+            else if() { }
+            else { }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Euroopa_riigid {
+    class Program
+    {
+        public static void Main() {
+
+            string readText = File.ReadAllText("C:\\Users\\opilane\\Source\\Repos\\Aleksander_Kaasik\\C# test\\Euroopa_riigid\\riigid_pealinnd.txt");
+            //Console.WriteLine(readText);
+
+            Dictionary<string, string> riik_pealinn = new Dictionary<string, string>();
+
+            foreach (string name in readText.Split("\n")){
+                string[] Euroopa = new string[2];
+                Euroopa = name.Replace("-ja-", "_ja_").Split("-");              
+                riik_pealinn[Euroopa[0]] = Euroopa[1];
+            }
+
+            Console.WriteLine( readText.Replace("-ja-", "-ja-").Split("-")[0] );
+
+            bool ProgramOnner = true;
+            Functions.Help();
+            while ( ProgramOnner ) 
+            {
+                string mode = Console.ReadLine();
+                if (mode == "" ){ mode = " "; }
+                switch (mode[0])
+                {
+                    case 'q': ProgramOnner = false; break;
+                    case 'h': Functions.Help();break;
+                    case 's': Functions.Show(riik_pealinn); break;
+                    default: Console.WriteLine("Error"); break;
+                };
+            }
+        }
+    }
+}
