97867bc920fa4c5336f49d58fdc53e9d728f4ab5
alexkaasik <aleks.kaasik@outlook.com>
2024-05-08 23:31:48 +0300

    Added function for add, save and some bit more functions

--- HelpMenu.cs
+++ HelpMenu.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.IO;
 
-namespace Euroopa_riigid
-{
-    class Functions
-    {
-        public static void Help()
-        {
+namespace Euroopa_riigid {
+    class Functions {
+
+        public static bool Contain( string sona, List<string> item ) {
+            foreach ( string value in item) {
+                if ( sona == value ) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static void Help() {
+            Console.WriteLine("h = Help");
             Console.WriteLine("f = Find");
             Console.WriteLine("s = Show");
             Console.WriteLine("e = Edit");
@@ -15,20 +24,87 @@ namespace Euroopa_riigid
             Console.WriteLine("c = save");
         }
 
-        public static void Show(Dictionary<string, string> item)
-        {
+        public static void Show(Dictionary<string, string> item) {
             foreach (var value in item) {
                 Console.WriteLine(value.Key + ":" + value.Value);
             }
         }
 
-        public static void Find(Dictionary<string, string> item)
-        {
-            Console.Write("Enter a word:");
-            string sona = Console.ReadLine().ToLower();
-            if ( dict.ContainsKey(sone) ) { } 
-            else if() { }
-            else { }
+        public static void Find(Dictionary<string, string> item) {
+
+            int index=0;
+            string sona = "";
+            string sona2="";
+            do {
+                Console.Write("Enter a word:");
+                sona = Console.ReadLine().ToLower();
+            } while (sona == "");
+
+            sona = char.ToUpper(sona[0]) + sona.Substring(1);
+            if ( Contain(sona, item.Keys.ToList() ) ) {
+                index = item.Keys.ToList().IndexOf(sona);
+                sona2 = item.Values.ToList()[index];
+                Console.WriteLine("riik -> pealinn");
+            }
+            else if ( Contain(sona, item.Values.ToList() ) ) {
+                index = item.Values.ToList().IndexOf(sona);
+                sona2 = item.Keys.ToList()[index];
+                Console.WriteLine("pealinn -> riik");
+            } 
+            else {  
+                // To add code
+                Console.WriteLine("Do you want add this country or city?");
+                char option = Console.ReadLine().ToLower()[0];
+                if (option == 'y'){
+                    Add(item, sona);
+                    option = 'n';
+                }
+            }
+
+            Console.WriteLine(char.ToUpper(sona[0] )+ sona.Substring(1) + " -> "  + sona2);
+        }
+        public static void Add(Dictionary<string, string> riik_pealinn, string sona) {
+            char option;
+            char IsThisRight;
+            string riik="";
+            string pealinn="";
+
+            do {
+                Console.Write("R/L: ");
+                option = Console.ReadLine().ToLower()[0];
+            } while ( option != 'r' && option != 'l' );
+            
+            do {
+                if ( option == 'l' ) {
+                    pealinn = sona;
+                    Console.Write("Riik nimi: ");
+                    riik = Console.ReadLine().ToLower();
+                }
+
+                if ( option == 'r' ) {
+                    riik = sona;
+                    Console.Write("Pealinn nimi: ");
+                    pealinn = Console.ReadLine().ToLower();
+                }
+                
+                Console.Write("Do you want add trhis word?:");
+                IsThisRight = Console.ReadLine().ToLower()[0];
+                if ( IsThisRight == 'y' ) {
+                    riik_pealinn[riik] = pealinn;
+                    Console.WriteLine("no");
+                }
+            }
+            while ( IsThisRight != 'y' && IsThisRight != 'q');
+            Console.WriteLine("no");
+        }
+
+        public static void Save(Dictionary<string, string> riik_pealinn) {
+            int ListMax = riik_pealinn.Keys.ToList().Count;
+            File.WriteAllText("riigid_pealinnd.txt", "");
+            for (int i = 0; i < ListMax; i++) {
+                string Name = riik_pealinn.Keys.ToList()[i]+"-"+riik_pealinn.Keys.ToList()[i];
+                File.AppendAllText("riigid_pealinnd.txt", Name+"\n");
+            }    
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,11 +2,9 @@
 using System.IO;
 
 namespace Euroopa_riigid {
-    class Program
-    {
+    class Program {
         public static void Main() {
-
-            string readText = File.ReadAllText("C:\\Users\\opilane\\Source\\Repos\\Aleksander_Kaasik\\C# test\\Euroopa_riigid\\riigid_pealinnd.txt");
+            string readText = File.ReadAllText("riigid_pealinnd.txt");
             //Console.WriteLine(readText);
 
             Dictionary<string, string> riik_pealinn = new Dictionary<string, string>();
@@ -14,22 +12,28 @@ namespace Euroopa_riigid {
             foreach (string name in readText.Split("\n")){
                 string[] Euroopa = new string[2];
                 Euroopa = name.Replace("-ja-", "_ja_").Split("-");              
-                riik_pealinn[Euroopa[0]] = Euroopa[1];
+                riik_pealinn[Euroopa[0]] = Euroopa[1].Remove(Euroopa[1].Length-1);
             }
 
-            Console.WriteLine( readText.Replace("-ja-", "-ja-").Split("-")[0] );
-
             bool ProgramOnner = true;
+            char mode = 'm';
+            string test;
+
             Functions.Help();
-            while ( ProgramOnner ) 
-            {
-                string mode = Console.ReadLine();
-                if (mode == "" ){ mode = " "; }
-                switch (mode[0])
-                {
-                    case 'q': ProgramOnner = false; break;
+            while ( ProgramOnner ) {
+                Console.Write( "switch(config)#" );
+                test = Console.ReadLine();
+                
+                if ( test == "" ){ test = " "; }
+                mode = Convert.ToChar(test);
+                switch (mode) {
                     case 'h': Functions.Help();break;
+                    case 'f': Functions.Find(riik_pealinn); break;
                     case 's': Functions.Show(riik_pealinn); break;
+                    case 'e': Console.WriteLine("Not aviable"); break;
+                    case 'g': Console.WriteLine("Not aviable"); break;
+                    case 'q': ProgramOnner = false; break;                 
+                    case 'c': Functions.Save(riik_pealinn); break;
                     default: Console.WriteLine("Error"); break;
                 };
             }
